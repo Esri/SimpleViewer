c5dc58ca833b78d1fc599c29353a8310b9ba207d
Kelly Hutchins <khutchins@esri.com>
2015-07-07 11:12:01 -0700

    Version updates

--- resources/configurationPanel.js
+++ resources/configurationPanel.js
@@ -1,7 +1,9 @@
 {
     "configurationSettings": [{
-        "category": "<b>Configure template</b>",
-        "fields": [{
+        "category": "<b>App settings</b>",
+        "fields": [            {
+               "type": "appproxies"
+            },{
             "type": "webmap"
         },{
             "placeHolder": "Defaults to web map title",
@@ -43,38 +45,18 @@
       "category": "Search Settings",
       "fields":[
             {
-                "type": "boolean",
-                "fieldName": "search",
-                "label": "Address Finder"
-            },{
-                "type": "boolean",
-                "fieldName": "searchExtent",
-                "label": "Search within current extent"
-            },{
                "type":"paragraph",
-               "value": "When Location Search is true the search widget will allow users to search for addresses and locations using one or more locators and also search the layers and fields specified in the Search Layers configuration option. Unchecking the Location Search option will remove the locator search and only configured search layers will be displayed."
-            },{
-               "type": "boolean",
-               "fieldName": "locationSearch",
-               "label": "Location Search"
-            },{  
-               "type":"paragraph",
-               "value":"Select search layers and fields. These layers will appear in the search tool allowing application users to search for particular values in the specified layers and fields."
+               "value": "Enable search to allow users to find a location or data in the map. Configure the search settings to refine the experience in your app by setting the default search resource, placeholder text, etc."
             },
             {
-               "label": "Select layers and fields",
-               "fieldName": "searchLayers",
-               "type": "multilayerandfieldselector",
-               "tooltip": "Select layer and fields to search",
-               "layerOptions":{
-                  "supportedTypes": ["FeatureLayer"],
-                  "geometryTypes": ["esriGeometryPoint", "esriGeometryLine", "esriGeometryPolyline", "esriGeometryPolygon"]
-               },
-               "fieldOptions":{
-                  "supportedTypes": ["esriFieldTypeString"]
-               }
+                "type": "boolean",
+                "fieldName": "search",
+                "label": "Enable search tool"
+            },{
+              "type": "search",
+              "fieldName": "searchConfig",
+              "label": "Configure search tool"
             }
-
       ]
     },{
         "category": "Tools",
@@ -90,11 +72,47 @@
             }
         ]
 
-    }],
+    },{
+         "category": "Custom URL Parameter",
+         "fields": [
+            {
+               "type": "paragraph",
+               "value": "Setup the app to support a custom url parameter. For example if your map contains a feature layer with parcel information and you'd like to be able to find parcels using a url parameter you can use this section to do so. Select a layer and search field then define the name of a custom param. Once you've defined these values you can append the custom search to your application url using the custom parameter name you define. For example, if I set the custom param value to parcels a custom url would look like this index.html?parcel=3045"
+            },{
+               "placeHolder":"i.e. parcels",
+               "label":"URL param name:",
+               "fieldName":"customUrlParam",
+               "type":"string",
+               "tooltip":"Custom URL param name"
+            },{  
+               "type":"layerAndFieldSelector",
+               "fieldName":"customUrlLayer",
+               "label":"Layer to search for custom url param value",
+               "tooltip":"Url param search layer",
+               "fields":[  
+                  {  
+                     "multipleSelection":false,
+                     "fieldName":"urlField",
+                     "label":"URL param search field",
+                     "tooltip":"URL param search field"
+                  }
+               ],
+               "layerOptions":{  
+                  "supportedTypes":[  
+                     "FeatureLayer"
+                  ],
+                  "geometryTypes":[  
+                     "esriGeometryPoint",
+                     "esriGeometryLine",
+                     "esriGeometryPolygon"
+                  ]
+               }
+            }
+
+         ]
+      }],
     "values": {
         "search": true,
-        "searchExtent": true,
-        "locationSearch": true,
         "locate": true,
         "color": "#2f4f4f",
         "theme": "#949494",
