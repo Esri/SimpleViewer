d4d9369128f9382e11da00210a98b192d25f3aeb
Kelly Hutchins <khutchins@esri.com>
2016-09-19 10:52:48 -0700

    4.3 updates

---
+++ js/nls/sr/resources.js
@@ -0,0 +1,16 @@
+define({
+  "map": {
+    "error": "Kreiranje mape nije moguće"
+  },
+  "tools": {
+    "search": {
+      "error": "Pronalaženje lokacije nije moguće",
+      "notWhatYouWanted": "Niste pronašli ono što ste tražili?",
+      "selectAnother": "Izaberite drugu lokaciju",
+      "currentLocation": "Trenutna lokacija",
+      "title": "Lokacija"
+    },
+    "legend": "Legenda",
+    "about": "Više informacija"
+  }
+});
